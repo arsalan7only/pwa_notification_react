af5749ad736ebf0a78600a5449064e22b666dd9a
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-01-22 17:58:48 +0530

    Arsalan : latest commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,51 +1,51 @@
-const PRECACHE = 'precache-v1';
-const RUNTIME = 'runtime';
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
 
 //Resources always being precached on load.
 const PRECACHE_URLS = [
-	'/index.html',
-	'/',
-	'/barcode192.png',
-	'/barcode256.png',
-	'/barcode384.png',
-	'/barcode512.png',
+  "/index.html",
+  "/",
+  "/barcode192.png",
+  "/barcode256.png",
+  "/barcode384.png",
+  "/barcode512.png",
 ];
 
 // Using install handler for precaching.
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
+    caches
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
       .then(self.skipWaiting())
   );
 });
 
 // Cleaning old cache using activate handler.
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   // const currentCaches = [PRECACHE, RUNTIME];
-  event.waitUntil( 
-     caches.keys().then(function(names) {
-          for (let name of names)   
-          {  
-              console.log("activate");
-              caches.delete(name);             
-          }
-          })
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      for (let name of names) {
+        console.log("activate");
+        caches.delete(name);
+      }
+    })
   );
 });
 
 // Populating runtime cache with response if no network is found.
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   // Skipping any made origin requests
   if (event.request.url.startsWith(self.location.origin)) {
     event.respondWith(
-      caches.match(event.request).then(cachedResponse => {
+      caches.match(event.request).then((cachedResponse) => {
         if (cachedResponse) {
           return cachedResponse;
         }
 
-        return caches.open(RUNTIME).then(cache => {
-          return fetch(event.request).then(response => {
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
               return response;
@@ -54,5 +54,41 @@ self.addEventListener('fetch', event => {
         });
       })
     );
+  }
+});
+
+self.addEventListener("push", (event) => {
+  const notification = event.data.text();
+  const convertParse = JSON.parse(notification);
+  const options = {
+    body: convertParse.body,
+    icon: convertParse.icon,
+    vibrate: [100, 50, 100],
+    data: { url: convertParse.url },
+    title: convertParse.title,
+  };
+
+  if (Object.keys(convertParse).length > 0) {
+    event.waitUntil(
+      self.registration.showNotification(
+        convertParse.title,
+        convertParse.notification
+      )
+    );
+  }
+});
+
+self.addEventListener("push", (event) => {
+  // Extract the unread count from the push message data.
+  const message = event.data.json();
+  const unreadCount = message.unreadCount;
+
+  // Set or clear the badge.
+  if (navigator.setAppBadge) {
+    if (unreadCount && unreadCount > 0) {
+      navigator.setAppBadge(unreadCount);
+    } else {
+      navigator.clearAppBadge();
+    }
   }
 });
