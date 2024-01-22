88228304e57b5054ffe8ebf6510c110126d65925
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-01-22 11:37:13 +0530

    Arsalan : testing app

--- public/sw.js
+++ public/sw.js
@@ -1,155 +1,3 @@
-// let cacheData = "appV1";
-// this.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     caches.open(cacheData).then((cache) => {
-//       cache.addAll([
-//         // "/static/js/main.chunk.js",
-//         // "/static/js/0.chunk.js",
-//         "/static/js/bundle.js",
-//         // "/static/css/main.chunk.css",
-//         // "/bootstrap.min.css",
-//         "/index.html",
-//         "/",
-//         // "/users",
-//       ]);
-//     })
-//   );
-// });
-// this.addEventListener("fetch", (event) => {
-//   // console.warn("url",event.request.url)
-
-// import { getData } from "../src/utils";
-
-//   if (!navigator.onLine) {
-//     if (event.request.url === "http://localhost:3000/static/js/main.chunk.js") {
-//       event.waitUntil(
-//         this.registration.showNotification("Internet", {
-//           body: "internet not working",
-//         })
-//       );
-//     }
-//     event.respondWith(
-//       caches.match(event.request).then((resp) => {
-//         if (resp) {
-//           return resp;
-//         }
-//         let requestUrl = event.request.clone();
-//         fetch(requestUrl);
-//       })
-//     );
-//   }
-// });
-
-// this.addEventListener("fetch", (event) => {
-//   //   debugger;
-//   let data = [
-//     { title: "Actify", body: "this is first " },
-//     { title: "Actify", body: "this is first " },
-//   ];
-//   //   fetch("https://jsonplaceholder.typicode.com/todos")
-//   //     .then((response) => response.json())
-//   //     .then((json) => localStorage.setItem("data", json));
-
-//   //   //   const getData = localStorage.getItem("data");
-//   //   console.log(getData);
-
-//   //   console.log(data);
-
-//   event.waitUntil(
-//     this.registration.showNotification("Actify", {
-//       body: "test",
-//       icon: "https://globalprimenews.com/wp-content/uploads/2020/03/IMG_20200326_143535-777x437.png",
-//     })
-//   );
-// });
-
-// public/service-worker.js
-
-const getData = () => {
-  debugger;
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.ready
-      .then((registration) => {
-        if (registration.pushManager) {
-          return registration.pushManager.getSubscription();
-        }
-        throw new Error("PushManager not available");
-      })
-      .then((subscription) => {
-        if (subscription === null) {
-          subscribeUser();
-        }
-      })
-      .catch((error) => {
-        console.error("Error checking pushManager:", error);
-      });
-  }
-
-  const subscribeUser = async () => {
-    try {
-      const registration = await navigator.serviceWorker.ready;
-      const convertedVapidKey = urlBase64ToUint8Array(
-        "BKdeaCCBxk3lnIJGRRHlhxKcF1kDyFiWeh0YX0Pfr6rXaPTEDWmL-E-h6vmbIXJntVnEhBNx6Y9QmBcbP5MyWAo"
-        // "BKdeaCCBxk3lnIJGRRHlhxKcF1kDyFiWeh0YX0Pfr6rXaPTEDWmL-E-h6vmbIXJntVnEhBNx6Y9QmBcbP5MyWAo"
-      );
-
-      const subscription = await registration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: convertedVapidKey,
-      });
-
-      console.log("User is subscribed:", subscription);
-
-      // Send the subscription to the server
-      sendSubscriptionToServer(subscription);
-    } catch (error) {
-      console.error("Failed to subscribe the user:", error);
-    }
-  };
-
-  const sendSubscriptionToServer = (subscription) => {
-    // Simulate sending the subscription to the server
-    // fetch(
-    //   // "http://localhost:5000/.netlify/functions/api/subscribe",
-    //   "https://react-notification-pwa-api.netlify.app/.netlify/functions/api/subscribe",
-    //   {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(subscription),
-    //   }
-    // )
-    //   .then((response) => response.json())
-    //   .then((data) => console.log("Subscription sent to server:", data))
-    //   .catch((error) => console.error("Error sending subscription:", error));
-    // const triggerPushNotification = () => {
-    const options = {
-      body: "Your Notification Body",
-    };
-
-    navigator.serviceWorker.ready.then((registration) => {
-      registration.showNotification("First Notification", options);
-    });
-    // };
-  };
-
-  const urlBase64ToUint8Array = (base64String) => {
-    const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding)
-      .replace(/-/g, "+")
-      .replace(/_/g, "/");
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-
-    for (let i = 0; i < rawData.length; ++i) {
-      outputArray[i] = rawData.charCodeAt(i);
-    }
-
-    return outputArray;
-  };
-};
-
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("appv1").then((cache) => {
@@ -165,7 +13,6 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  console.log("i am call");
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
@@ -181,13 +28,27 @@ self.addEventListener("push", (event) => {
     icon: convertParse.icon,
     vibrate: [100, 50, 100],
     data: { url: convertParse.url },
-    badge: 5,
+    title: convertParse.title,
   };
-  console.log("options", notification);
 
   if (Object.keys(convertParse).length > 0) {
     event.waitUntil(
       self.registration.showNotification(convertParse.title, options)
     );
+  }
+});
+
+self.addEventListener("push", (event) => {
+  // Extract the unread count from the push message data.
+  const message = event.data.json();
+  const unreadCount = message.unreadCount;
+
+  // Set or clear the badge.
+  if (navigator.setAppBadge) {
+    if (unreadCount && unreadCount > 0) {
+      navigator.setAppBadge(unreadCount);
+    } else {
+      navigator.clearAppBadge();
+    }
   }
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-// src/App.js
 import React, { useEffect, useState } from "react";
-
+import "./App.css";
 const App = () => {
-  const [count, setCount] = useState(0);
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker.ready
@@ -30,40 +30,17 @@ const App = () => {
       const registration = await navigator.serviceWorker.ready;
       const convertedVapidKey = urlBase64ToUint8Array(
         "BKdeaCCBxk3lnIJGRRHlhxKcF1kDyFiWeh0YX0Pfr6rXaPTEDWmL-E-h6vmbIXJntVnEhBNx6Y9QmBcbP5MyWAo"
-        // "BKdeaCCBxk3lnIJGRRHlhxKcF1kDyFiWeh0YX0Pfr6rXaPTEDWmL-E-h6vmbIXJntVnEhBNx6Y9QmBcbP5MyWAo"
       );
 
       const subscription = await registration.pushManager.subscribe({
         userVisibleOnly: true,
         applicationServerKey: convertedVapidKey,
       });
-      // sendSubscriptionToServer(subscription);
       console.log("User is subscribed:", subscription);
       return subscription;
-
-      // Send the subscription to the server
-      // sendSubscriptionToServer(subscription);
     } catch (error) {
       console.error("Failed to subscribe the user:", error);
     }
-  };
-
-  const sendSubscriptionToServer = (subscription) => {
-    // Simulate sending the subscription to the server
-    fetch(
-      "http://localhost:5000/.netlify/functions/api/subscribe",
-      // "https://react-notification-pwa-api.netlify.app/.netlify/functions/api/subscribe",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(subscription),
-      }
-    )
-      .then((response) => response.json())
-      .then((data) => console.log("Subscription sent to server:", data))
-      .catch((error) => console.error("Error sending subscription:", error));
   };
 
   const urlBase64ToUint8Array = (base64String) => {
@@ -82,7 +59,6 @@ const App = () => {
   };
 
   const handleSubmit = async (e) => {
-    debugger;
     e.preventDefault();
     let test = await subscribeUser();
     console.log("test", test);
@@ -91,64 +67,54 @@ const App = () => {
       password,
       token: test,
     };
-    fetch(
-      // "http://localhost:5000/.netlify/functions/api/login",
-      "https://node-mongo-api-g1v4.onrender.com/api/v1/login",
-      // "https://react-notification-pwa-api.netlify.app/.netlify/functions/api/login",
-      // "https://react-notification-pwa-api.netlify.app/.netlify/functions/api/subscribe",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(payload),
+    fetch("https://node-mongo-api-g1v4.onrender.com/api/v1/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    }).then((res) => {
+      if (res.status == 200 && res.data !== "inviled credential") {
+        setIsLoggedIn(true);
       }
-    ).then((res) => {
       console.log("test", res);
     });
   };
 
   return (
     <div className="App">
-      <form>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-            placeholder="Enter email"
-            onChange={(e) => setUserName(e.target.value)}
-          />
+      {!isLoggedIn ? (
+        <div class="container">
+          <div class="card">
+            <h2>Login</h2>
+            <form>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Username"
+                onChange={(e) => setUserName(e.target.value)}
+                required
+              />
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <button type="submit" onClick={(e) => handleSubmit(e)}>
+                Login
+              </button>
+            </form>
+          </div>
         </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Password</label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+      ) : (
+        <div>
+          <h3>Welcome</h3>
         </div>
-        <div class="form-group form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">
-            Check me out
-          </label>
-        </div>
-        <button class="btn btn-primary" onClick={(e) => handleSubmit(e)}>
-          Submit
-        </button>
-      </form>
-      <h1>React PWA with Push Notifications</h1>
-      <button
-        onClick={subscribeUser}
-        style={{ width: "300px", backgroundColor: "red" }}
-      >
-        Enable Push Notifications
-      </button>
+      )}
     </div>
   );
 };
