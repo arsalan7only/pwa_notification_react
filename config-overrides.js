b6daf6cd1d3522b4102fa7cbfab5efc614faa4c6
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-04-30 02:07:27 +0530

    Arsalan : wild card

--- config-overrides.js
+++ config-overrides.js
@@ -90,14 +90,9 @@ const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const { webpack } = require("react-app-rewired/config-overrides");
 
 const cspConfigPolicy = {
-  "default-src": ["'self'"],
+  "default-src": "self",
   "script-src": ["'self' 'unsafe-inline'"],
   "style-src": ["'self' 'unsafe-inline'"],
-  "object-src": "'none'",
-  "img-src": ["'*'"],
-  "media-src": ["'self'"],
-  "font-src": ["'self'"],
-  "frame-ancestors": ["'none'"],
 };
 
 function addCspHtmlWebPackPlugin(config) {
