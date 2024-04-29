76e93d55bc730a0f45195158a708337abf70833c
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-04-30 00:58:56 +0530

    arsalan : added csp header

---
+++ config-overrides.js
@@ -0,0 +1,52 @@
+// const { override } = require("customize-cra");
+// const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+
+// module.exports = override((config) => {
+//   // Add the CSP plugin to the Webpack config
+//   config.plugins.push(
+//     new cspHtmlWebpackPlugin({
+//       "default-src": "'self'",
+//       "script-src": ["'self'"],
+//       "style-src": ["'self'"],
+//       // Add more directives as needed
+//       hashEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//       nonceEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//     })
+//   );
+
+//   return config;
+// });
+
+const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Other webpack configuration options...
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      // Add other options as needed
+    }),
+    new CspHtmlWebpackPlugin({
+      "default-src": "'self'",
+      "script-src": ["'self'"],
+      "style-src": ["'self'"],
+      // Add more directives as needed
+      hashEnabled: {
+        "script-src": true,
+        "style-src": true,
+      },
+      nonceEnabled: {
+        "script-src": true,
+        "style-src": true,
+      },
+    }),
+  ],
+};
