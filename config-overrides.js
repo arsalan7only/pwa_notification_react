9c4c07ad1daa6e73d3d2e6c3e3a83aff08db6c69
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-04-30 01:15:45 +0530

    Arsalan new try csp

--- config-overrides.js
+++ config-overrides.js
@@ -51,36 +51,61 @@
 //   ],
 // };
 
-const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//   // Other webpack configuration options...
+//   devServer: {
+//     // Other dev server options...
+//     headers: {
+//       "Content-Security-Policy":
+//         "default-src 'self'; script-src 'self'; style-src 'self';",
+//     },
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./public/index.html",
+//       // Add other options as needed
+//     }),
+//     new CspHtmlWebpackPlugin({
+//       "default-src": "'self'",
+//       "script-src": ["'self'"],
+//       "style-src": ["'self'"],
+//       // Add more directives as needed
+//       hashEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//       nonceEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//     }),
+//   ],
+// };
+
+const { override } = require("customize-cra");
+const cspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+const { webpack } = require("react-app-rewired/config-overrides");
+
+const cspConfigPolicy = {
+  "default-src": "'none'",
+  "script-src": ["'self'"],
+  "style-src": ["'self'"],
+  "object-src": "'none'",
+  "img-src": ["'self'"],
+  "media-src": ["'self'"],
+  "font-src": ["'self'"],
+};
+
+function addCspHtmlWebPackPlugin(config) {
+  if (process.env.NODE_ENV == "production") {
+    config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
+  }
+  return config;
+}
 
 module.exports = {
-  // Other webpack configuration options...
-  devServer: {
-    // Other dev server options...
-    headers: {
-      "Content-Security-Policy":
-        "default-src 'self'; script-src 'self'; style-src 'self';",
-    },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      // Add other options as needed
-    }),
-    new CspHtmlWebpackPlugin({
-      "default-src": "'self'",
-      "script-src": ["'self'"],
-      "style-src": ["'self'"],
-      // Add more directives as needed
-      hashEnabled: {
-        "script-src": true,
-        "style-src": true,
-      },
-      nonceEnabled: {
-        "script-src": true,
-        "style-src": true,
-      },
-    }),
-  ],
+  webpack: override(addCspHtmlWebPackPlugin),
 };
