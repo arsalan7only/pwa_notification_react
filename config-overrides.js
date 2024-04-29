5da1665acaac9d6a8f88cb81eda843c5320aeebd
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-04-30 01:05:28 +0530

    Arsalan latest csp

--- config-overrides.js
+++ config-overrides.js
@@ -23,12 +23,46 @@
 //   return config;
 // });
 
+// const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//   // Other webpack configuration options...
+
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "./public/index.html",
+//       // Add other options as needed
+//     }),
+//     new CspHtmlWebpackPlugin({
+//       "default-src": "'self'",
+//       "script-src": ["'self'"],
+//       "style-src": ["'self'"],
+//       // Add more directives as needed
+//       hashEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//       nonceEnabled: {
+//         "script-src": true,
+//         "style-src": true,
+//       },
+//     }),
+//   ],
+// };
+
 const CspHtmlWebpackPlugin = require("csp-html-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   // Other webpack configuration options...
-
+  devServer: {
+    // Other dev server options...
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self'; style-src 'self';",
+    },
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
