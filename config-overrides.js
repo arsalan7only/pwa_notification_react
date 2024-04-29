185cc90664313338d136abeb648863b01baa0626
MOHAMMED Waseem <arsalan8only@gmail.com>
2024-04-30 01:23:29 +0530

    Arsalan new try csps

--- config-overrides.js
+++ config-overrides.js
@@ -100,7 +100,7 @@ const cspConfigPolicy = {
 };
 
 function addCspHtmlWebPackPlugin(config) {
-  if (process.env.NODE_ENV == "development") {
+  if (process.env.NODE_ENV == "production") {
     config.plugins.push(new cspHtmlWebpackPlugin(cspConfigPolicy));
   }
   return config;
